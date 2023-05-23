98d71f89021e7eb1908457e5ff804bebd2a9a360
Yusu-f <yusouphishola01@gmail.com>
2023-05-23 19:52:35 +0100

    y

--- netlify/functions/fetch-sub/fetch-sub.js
+++ netlify/functions/fetch-sub/fetch-sub.js
@@ -3,17 +3,15 @@ import { YoutubeTranscript } from "youtube-transcript";
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    let transcript = await YoutubeTranscript.fetchTranscript("A-PFgCM4jwM")
+    let transcript = await YoutubeTranscript.fetchTranscript("A-PFgCM4jwM");
 
-    
-      return {
-        statusCode: 200,
-        body: transcript[0],
-        // // more keys you can return:
-        // headers: { "headerName": "headerValue", ... },
-        // isBase64Encoded: true,
-      };
-    
+    return {
+      statusCode: 200,
+      body: transcript[0].text,
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    };
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
   }
