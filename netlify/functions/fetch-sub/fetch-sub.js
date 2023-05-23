310ea0429c6df3cf85765ea6a94ec72a22c5fd63
Yusu-f <yusouphishola01@gmail.com>
2023-05-23 19:48:29 +0100

    y

--- netlify/functions/fetch-sub/fetch-sub.js
+++ netlify/functions/fetch-sub/fetch-sub.js
@@ -13,7 +13,7 @@ const handler = async (event) => {
         // headers: { "headerName": "headerValue", ... },
         // isBase64Encoded: true,
       };
-    });
+    
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
   }
