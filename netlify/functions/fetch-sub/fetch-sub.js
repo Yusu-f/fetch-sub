9c46b0a3e706588687e1b269d218782308c9c06f
Yusu-f <yusouphishola01@gmail.com>
2023-05-23 18:52:52 +0100

    added func

--- netlify/functions/fetch-sub/fetch-sub.js
+++ netlify/functions/fetch-sub/fetch-sub.js
@@ -1,17 +1,26 @@
+import { YoutubeTranscript } from "youtube-transcript";
+
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    let transcript = "";
+
+    YoutubeTranscript.fetchTranscript("A-PFgCM4jwM").then((transcriptArray) => {
+      transcriptArray.forEach((e) => {
+        console.log(e.text);
+        transcript += e.text.replace(/\n/g, "");
+      });
+    });
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: transcript,
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
-    }
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
