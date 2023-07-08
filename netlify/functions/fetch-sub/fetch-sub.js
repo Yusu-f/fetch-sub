e0778f166f31b18988c3d435c033c46dc476235c
Yusu-f <yusouphishola01@gmail.com>
2023-07-08 19:17:55 +0100

    n

--- netlify/functions/fetch-sub/fetch-sub.js
+++ netlify/functions/fetch-sub/fetch-sub.js
@@ -3,20 +3,32 @@ import { YoutubeTranscript } from "youtube-transcript";
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    let transcript = ""
-    const transcriptArray = await YoutubeTranscript.fetchTranscript(event.queryStringParameters.v);
-    
-    transcriptArray.forEach(i => {
-      transcript += i.text + " "
-    })
+    if (event.path == "/fetchjobs") {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ transcript: "fetchnjobs hit" }),
+        Headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      };
+    } else {
+      let transcript = "";
+      const transcriptArray = await YoutubeTranscript.fetchTranscript(
+        event.queryStringParameters.v
+      );
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ transcript: transcript}),
-      Headers: {
-        "Access-Control-Allow-Origin": "*"
-      }
-    };
+      transcriptArray.forEach((i) => {
+        transcript += i.text + " ";
+      });
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ transcript: transcript }),
+        Headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      };
+    }
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
   }
