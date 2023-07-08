918828981fb93306ca3f98469feb5dfd2bd791f4
Yusu-f <yusouphishola01@gmail.com>
2023-07-08 19:21:04 +0100

    n

--- netlify/functions/fetch-sub/fetch-sub.js
+++ netlify/functions/fetch-sub/fetch-sub.js
@@ -3,6 +3,7 @@ import { YoutubeTranscript } from "youtube-transcript";
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
+    console.log(event.path);
     if (event.path == "/fetchjobs") {
       return {
         statusCode: 200,
