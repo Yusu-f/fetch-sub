6bf7a96262cc49e442a792093e61c280f84029ab
Yusu-f <yusouphishola01@gmail.com>
2023-07-08 19:31:38 +0100

    n

--- netlify/functions/fetch-jobs/fetch-jobs.js
+++ netlify/functions/fetch-jobs/fetch-jobs.js
@@ -3,16 +3,10 @@ import { YoutubeTranscript } from "youtube-transcript";
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    let transcript = ""
-    const transcriptArray = await YoutubeTranscript.fetchTranscript(event.queryStringParameters.v);
-    
-    transcriptArray.forEach(i => {
-      transcript += i.text + " "
-    })
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ transcript: transcript}),
+      body: JSON.stringify({ transcript: "fetchjobs"}),
       Headers: {
         "Access-Control-Allow-Origin": "*"
       }
