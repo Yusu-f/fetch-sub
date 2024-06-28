748ae140ce1cccce034d4ad06546eeca3ec6aa75
Yusu-f <yusouphishola01@gmail.com>
2024-06-28 10:14:49 +0100

    fixed broken dep

--- netlify/functions/fetch-sub/fetch-sub.js
+++ netlify/functions/fetch-sub/fetch-sub.js
@@ -1,44 +1,26 @@
 import { YoutubeTranscript } from "youtube-transcript";
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-// const handler = async (event) => {
-//   try {
-//     let transcript = ""
-//     const transcriptArray = await YoutubeTranscript.fetchTranscript(event.queryStringParameters.v);
+const handler = async (event) => {
+  try {
+    let transcript = ""
+    const transcriptArray = await YoutubeTranscript.fetchTranscript(event.queryStringParameters.v);
 
-//     transcriptArray.forEach(i => {
-//       transcript += i.text + " "
-//     })
+    transcriptArray.forEach(i => {
+      transcript += i.text + " "
+    })
 
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({ transcript: transcript}),
-//       headers: {
-//         "Access-Control-Allow-Origin": "https://chatgpt.com/"
-//       }
-//     };
-//   } catch (error) {
-//     return { statusCode: 500, body: error.toString() };
-//   }
-// };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ transcript: transcript}),
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
 
-// module.exports = { handler };
+module.exports = { handler };
 
-try {
-  let transcript = ""
-  const transcriptArray = await YoutubeTranscript.fetchTranscript(event.queryStringParameters.v);
-
-  transcriptArray.forEach(i => {
-    transcript += i.text + " "
-  })
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ transcript: transcript}),
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  };
-} catch (error) {
-  return { statusCode: 500, body: error.toString() };
-}
